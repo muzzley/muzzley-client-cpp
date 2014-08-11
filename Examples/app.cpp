5ef2d56177cb14aa16016b9ea61f32cbaf927377
naazgull <pedro.figueiredo@gmail.com>
2014-08-11 19:31:18 +0100

    added switch of LED13 to app.cpp example

--- Examples/app.cpp
+++ Examples/app.cpp
@@ -53,8 +53,37 @@ int main(int argc, char* argv[]) {
 	//
 	// Return 'false' if you want to stop other listeners from being invoked.
 	_client.on(muzzley::SignalingMessage, [] (muzzley::JSONObj& _data, muzzley::Client& _client) -> bool {
-		cout << (bool) _data["s"] << endl << flush;
-		cout << (string) _data["d"]["a"] << endl << flush;
+
+		// Switch on the Intel(R) Galileo LED 13
+		// (pure c++ version)
+		ofstream _export;
+		_export.open("/sys/class/gpio/export", ios::out | ios::trunc);
+		ofstream _direction;
+		_direction.open("/sys/class/gpio/gpio3/direction", ios::out | ios::trunc);
+		ofstream _value;
+		_value.open("/sys/class/gpio/gpio3/value", ios::out | ios::trunc);
+
+		_export << "3" << flush;
+		_export.close();
+
+		_direction << "out" << flush;
+		_direction.close();
+
+		_value << "1" << flush;
+		_value.close();
+
+		// Switch on the Intel(R) Galileo LED 13
+		// (syscall version)
+		if (system("echo 3 > /sys/class/gpio/export") == -1) {
+			cout << "error" << endl << flush;
+		}
+		if (system("echo out > /sys/class/gpio/gpio3/direction") == -1) {
+			cout << "error" << endl << flush;
+		}
+		if (system("echo 1 > /sys/class/gpio/gpio3/value") == -1) {
+			cout << "error" << endl << flush;
+		}
+
 		return true;
 	});
 
