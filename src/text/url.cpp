a37d660954a566f7b852d76dea1bd6ad938cb61c
naazgull <pedro.figueiredo@gmail.com>
2015-02-06 17:58:29 +0000

    corrected url_encode function to conform with rfc1738

--- src/text/url.cpp
+++ src/text/url.cpp
@@ -27,6 +27,24 @@ using namespace std;
 using namespace __gnu_cxx;
 #endif
 
+/*
+   Unsafe:
+
+   Characters can be unsafe for a number of reasons.  The space
+   character is unsafe because significant spaces may disappear and
+   insignificant spaces may be introduced when URLs are transcribed or
+   typeset or subjected to the treatment of word-processing programs.
+   The characters "<" and ">" are unsafe because they are used as the
+   delimiters around URLs in free text; the quote mark (""") is used to
+   delimit URLs in some systems.  The character "#" is unsafe and should
+   always be encoded because it is used in World Wide Web and in other
+   systems to delimit a URL from a fragment/anchor identifier that might
+   follow it.  The character "%" is unsafe because it is used for
+   encodings of other characters.  Other characters are unsafe because
+   gateways and other transport agents are known to sometimes modify
+   such characters. These characters are "{", "}", "|", "\", "^", "~",
+   "[", "]", and "`".
+*/
 void muzzley::url_encode(string& _out) {
 	const char DEC2HEX[16 + 1] = "0123456789ABCDEF";
 	const unsigned char * pSrc = (const unsigned char *) _out.c_str();
@@ -36,7 +54,7 @@ void muzzley::url_encode(string& _out) {
 	const unsigned char * const SRC_END = pSrc + SRC_LEN;
 
 	for (; pSrc < SRC_END; ++pSrc) {
-		if ((*pSrc > 127) || (*pSrc == '%') || (*pSrc == ' ') || (*pSrc == '&') || (*pSrc == '+') || (*pSrc == '?') || (*pSrc == '#') || (*pSrc == '=') || (*pSrc == '/') || (*pSrc == ':')) {
+		if ((*pSrc > 127) || (*pSrc == '"') || (*pSrc == '{') || (*pSrc == '}') || (*pSrc == '|') || (*pSrc == '\\') || (*pSrc == '^') || (*pSrc == '~') || (*pSrc == '[') || (*pSrc == ']') || (*pSrc == '`') || (*pSrc == '%') || (*pSrc == ' ') || (*pSrc == '&') || (*pSrc == '+') || (*pSrc == '?') || (*pSrc == '#') || (*pSrc == '=') || (*pSrc == '/') || (*pSrc == ':')) {
 			*pEnd++ = '%';
 			*pEnd++ = DEC2HEX[*pSrc >> 4];
 			*pEnd++ = DEC2HEX[*pSrc & 0x0F];
